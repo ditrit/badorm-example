2ece5eb6b548b75e21167735be98c4cf797035c7
Franco Liberali <franco.liberali@gmail.com>
2023-05-17 11:01:53 +0200

    make example capable of being runned multiple times

--- example.go
+++ example.go
@@ -14,53 +14,85 @@ import (
 func CreateCRUDObjects(
 	db *gorm.DB,
 	crudProductRepository badorm.CRUDRepository[models.Product, uuid.UUID],
-) []*models.Product {
-	log.Println("Setting up CRUD example")
+) ([]*models.Product, error) {
+	products, err := crudProductRepository.GetAll(db)
+	if err != nil {
+		return nil, err
+	}
 
-	product1 := &models.Product{
-		Int: 1,
-	}
-	_ = crudProductRepository.Create(db, product1)
+	if len(products) == 0 {
+		log.Println("Setting up CRUD example")
 
-	product2 := &models.Product{
-		Int: 2,
-	}
-	_ = crudProductRepository.Create(db, product2)
+		product1 := &models.Product{
+			Int: 1,
+		}
+		err = crudProductRepository.Create(db, product1)
+		if err != nil {
+			return nil, err
+		}
 
-	company1 := &models.Company{
-		Name: "ditrit",
-	}
-	_ = db.Create(company1).Error
-	company2 := &models.Company{
-		Name: "orness",
-	}
-	_ = db.Create(company2).Error
+		product2 := &models.Product{
+			Int: 2,
+		}
+		err = crudProductRepository.Create(db, product2)
+		if err != nil {
+			return nil, err
+		}
 
-	seller1 := &models.Seller{
-		Name:      "franco",
-		CompanyID: &company1.ID,
-	}
-	_ = db.Create(seller1).Error
-	seller2 := &models.Seller{
-		Name:      "agustin",
-		CompanyID: &company2.ID,
-	}
-	_ = db.Create(seller2).Error
+		company1 := &models.Company{
+			Name: "ditrit",
+		}
+		err = db.Create(company1).Error
+		if err != nil {
+			return nil, err
+		}
+		company2 := &models.Company{
+			Name: "orness",
+		}
+		err = db.Create(company2).Error
+		if err != nil {
+			return nil, err
+		}
 
-	sale1 := &models.Sale{
-		Product: product1,
-		Seller:  seller1,
-	}
-	_ = db.Create(sale1).Error
-	sale2 := &models.Sale{
-		Product: product2,
-		Seller:  seller2,
-	}
-	_ = db.Create(sale2).Error
+		seller1 := &models.Seller{
+			Name:      "franco",
+			CompanyID: &company1.ID,
+		}
+		err = db.Create(seller1).Error
+		if err != nil {
+			return nil, err
+		}
+		seller2 := &models.Seller{
+			Name:      "agustin",
+			CompanyID: &company2.ID,
+		}
+		err = db.Create(seller2).Error
+		if err != nil {
+			return nil, err
+		}
 
-	log.Println("Finished creating CRUD example")
+		sale1 := &models.Sale{
+			Product: product1,
+			Seller:  seller1,
+		}
+		err = db.Create(sale1).Error
+		if err != nil {
+			return nil, err
+		}
+		sale2 := &models.Sale{
+			Product: product2,
+			Seller:  seller2,
+		}
+		err = db.Create(sale2).Error
+		if err != nil {
+			return nil, err
+		}
 
-	return []*models.Product{product1, product2}
+		log.Println("Finished creating CRUD example")
+		return []*models.Product{product1, product2}, nil
+	}
+
+	return nil, nil
 }
 
 func QueryCRUDObjects(
